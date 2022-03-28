88eb0feffef3e8fb8aeabc2fea00c9a4909a4b26
Facundo Barrera <barreraaybarfacundo@gmail.com>
2022-03-28 20:24:18 -0300

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,23 @@
-const reverseString = function() {
+const reverseString = function(string)
+{
+    let reversedString, arrayString, arrayStringReversed;
 
+    arrayString = string.split("");
+    arrayStringReversed = new Array(arrayString.length);
+    
+    let front = 0, end = arrayString.length;
+    while (front < end)
+    {
+        arrayStringReversed[front] = arrayString[end];
+        arrayStringReversed[end] = arrayString[front];
+
+        front++;
+        end--;
+    }
+
+    reversedString = arrayStringReversed.join("");
+
+    return reversedString;
 };
 
 // Do not edit below this line
