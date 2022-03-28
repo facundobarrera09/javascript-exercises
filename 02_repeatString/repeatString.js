18c73932262c3f7e98fe4a719a2c61b06eca053c
Facundo Barrera <barreraaybarfacundo@gmail.com>
2022-03-28 20:14:07 -0300

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num)
+{
+    let repeatedString = "";
 
+    if (num >= 0)
+        for (let x = 0; x < num; x++)
+            repeatedString += string;
+    else
+        repeatedString = "ERROR";
+
+    return repeatedString;
 };
 
 // Do not edit below this line
