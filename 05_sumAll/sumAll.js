d71a2b14c70a03201105a277796fdf22ee030b99
Facundo Barrera <barreraaybarfacundo@gmail.com>
2022-03-28 20:52:47 -0300

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, last)
+{
+    let sum = 0, aux;
 
+    if (first < 0 || last < 0 || typeof first != "number" || typeof last != "number")
+        return "ERROR";
+
+    if (last < first)
+    {
+        aux = last;
+        last = first;
+        first = aux;
+    }
+
+    for (let x = first; x <= last; x++)
+        sum += x;
+
+
+    return sum;
 };
 
 // Do not edit below this line
