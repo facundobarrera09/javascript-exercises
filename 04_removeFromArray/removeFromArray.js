900507e94c857ec61dfd72985c199bcdf6013a3a
Facundo Barrera <barreraaybarfacundo@gmail.com>
2022-03-28 20:46:14 -0300

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args)
+{
+    for (let arg = 0; arg < args.length; arg++)
+    {
+        for (let x = 0; x < array.length; x++)
+        {
+            if (array[x] === args[arg])
+                array.splice(x, 1);
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
