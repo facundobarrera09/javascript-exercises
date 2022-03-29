e38c2d0e53ce8e50c155ae4707aeb031a543f0b3
Facundo Barrera <barreraaybarfacundo@gmail.com>
2022-03-28 21:03:31 -0300

    Solve exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year)
+{
+    let isLeapYear;
+    let isDivBy4 = (year % 4 == 0);
+    let isDivBy100 = (year % 100 == 0);
+    let isDivBy400 = (year % 400 == 0);
 
+    if ((isDivBy4 && !isDivBy100) || (isDivBy100 && isDivBy400))
+        isLeapYear = true;
+    else
+        isLeapYear = false;
+
+    return isLeapYear;
 };
 
 // Do not edit below this line
