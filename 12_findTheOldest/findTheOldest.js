2486132c1a0faae13f826f0859cb58060de5f4ad
Facundo Barrera <barreraaybarfacundo@gmail.com>
2022-04-01 20:31:21 -0300

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people)
+{
+    return people.reduce((obj, item) => {
+        if (obj === undefined) return item;
 
+        let objAge = (('yearOfDeath' in obj) ? obj.yearOfDeath : 2022)
+                        - obj.yearOfBirth;
+        let itemAge = (('yearOfDeath' in item) ? item.yearOfDeath : 2022)
+                        - item.yearOfBirth;
+
+        if (itemAge > objAge) return item;
+
+        return obj;
+    }, undefined);
 };
 
 // Do not edit below this line
