838f40f6481ba23ba73d89820425d38b4bd3e58c
Facundo Barrera <barreraaybarfacundo@gmail.com>
2022-04-01 19:43:11 -0300

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(pos)
+{
+    if (pos < 0) return "OOPS";
 
+    const aurea = (1 + Math.sqrt(5)) / 2;
+    const num = (1 / Math.sqrt(5)) * (Math.pow(aurea, pos) - Math.pow(-1 / aurea, pos));
+
+    return Math.trunc(num);
 };
 
 // Do not edit below this line
